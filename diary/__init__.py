6164b979af93982712383b1f88d05d06b245b930
Ali Atieh <atieha@cardiff.ac.uk>
2022-10-11 13:05:58 +0100

    Initial commit

---
+++ diary/__init__.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '5b28703b32c828ab64990c617b4e567b62076e79038a8688'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c1809817:Shadow28469@csmysql.cs.cf.ac.uk:3306/c1809817_digital_Diary' 
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print('Created Database!')
